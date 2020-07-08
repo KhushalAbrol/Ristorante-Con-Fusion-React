c4f16334412637b0047bed764a2a4fbe21820d64
KhushalAbrol <khushalabrol14@gmail.com>
2020-07-08 09:22:42 +0530

    DishDetailComponent Fixed

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -41,16 +42,27 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     const DishDetail = (props) => {
-        const dish = props.selectedDish;
+        const dish = props.dish;
+        const comments = props.comments;
         if (dish != null){
             return(
                 <div className="container">
                     <div className="row">
-                        <RenderDish dish={dish}/>
-                        <RenderComments comments={dish.comments} />
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        <RenderDish dish={dish} />
+                        <RenderComments comments={comments} />
                     </div>
                 </div>
-            )
+            );            
         }
         else
             return(
