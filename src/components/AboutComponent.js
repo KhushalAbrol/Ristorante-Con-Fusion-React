0856a871f9c5c2493e6ac65e85a6e0198f5257b0
KhushalAbrol <khushalabrol14@gmail.com>
2020-07-08 14:04:54 +0530

    Fix: RenderLeader Function Added

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+const RenderLeader = ({leader}) => {
+    return(
+        <div key={leader.id} className="col-12 mt-5">
+            <Media tag="li">
+            <Media left middle>
+                    <Media object src={leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+            </Media>
+        </div>
+    )
+}
 function About(props) {
      const leaders = props.leaders.map((leader) => {
         return (
-            <div key={leader.id} className="col-12 mt-5">
-                <Media tag="li">
-                <Media left middle>
-                        <Media object src={leader.image} alt={leader.name} />
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading>{leader.name}</Media>
-                        <p>{leader.designation}</p>
-                        <p>{leader.description}</p>
-                    </Media>
-                </Media>
-            </div>
+            <RenderLeader leader={leader} />
         );
     });
 
