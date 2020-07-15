230342fbb0f5bbe93e1e446a77013d121ea1bb85
KhushalAbrol <khushalabrol14@gmail.com>
2020-07-15 08:29:27 +0530

    Assignment 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
-const RenderLeader = ({leader}) => {
-    return(
-        <div key={leader.id} className="col-12 mt-5">
-            <Media tag="li">
-            <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} />
-                </Media>
-                <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                </Media>
-            </Media>
-        </div>
-    )
+function RenderLeader({leader}) {
+
+	return(
+		<div className="col-12 mt-5">
+			<Fade in>
+			<Media tag="li">
+				<Media left middle>
+					<Media object src={baseUrl + leader.image} alt={leader.name} />
+				</Media>
+				<Media body className="col-12">
+					<Media heading>{leader.name}</Media>
+					<p>{leader.designation}</p>
+					<p>{leader.description}</p>
+				</Media>
+			</Media>
+			</Fade>
+		</div>
+	);
 }
-function About(props) {
-     const leaders = props.leaders.map((leader) => {
+
+const About = (props) => {
+
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
-            <RenderLeader leader={leader} />
+            <RenderLeader leader={leader} key={leader.id} />
         );
     });
 
@@ -35,7 +43,7 @@ function About(props) {
                 <div className="col-12">
                     <h3>About Us</h3>
                     <hr />
-                </div>                
+                </div>
             </div>
             <div className="row row-content">
                 <div className="col-12 col-md-6">
@@ -67,8 +75,8 @@ function About(props) {
                                 <p className="mb-0">You better cut the pizza in four pieces because
                                     I'm not hungry enough to eat six.</p>
                                 <footer className="blockquote-footer">Yogi Berra,
-                                <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
-                                    P. Pepe, Diversion Books, 2014</cite>
+                                    <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
+                                        P. Pepe, Diversion Books, 2014</cite>
                                 </footer>
                             </blockquote>
                         </CardBody>
@@ -81,7 +89,9 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
+						<Stagger in>
                         {leaders}
+						</Stagger>
                     </Media>
                 </div>
             </div>
@@ -89,4 +99,4 @@ function About(props) {
     );
 }
 
-export default About;    
+export default About;
